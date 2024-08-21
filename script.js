b73952f6ffc46f3e7e727217898a60001c209a0a
tamyrhuana <tamyres.rsmagalhaes@senacsp.edu.br>
2024-08-21 20:06:43 -0300

    feat: Calcular a média de dois  alunos

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
-// Exibir no terminl
-console.log("Olá Mundo");
-// Exibir no navegador
-document.write("Olá Navegador! 4");
-// Exibir alerta
-alert("Alerta de Confirmação");
 /*
-Comentario
-de 
-Bloco
-tudo que fica entre /*     /*
+"Faça um algoritmo onde o usuário informe o nome
+e 4 notas de dois alunos e mostre as
+médias desses 2 alunos."
 */
+var nome = window.prompt("Informe o nome: ");
 
-// capturar o nome do Aluno
-var nome = window.prompt("Informe o nome:");
-
-// Guardar a primeira nota1
 var nota1 = window.prompt("Digite a nota 1:");
-
-// Converter a nota1 de texto para decimal
 nota1 = parseFloat(nota1);
 
-// Guardar a segunda nota2
-var nota2 = window.prompt("Digite a nota 2");
+var nota2 = window.prompt("Digite a nota 2:");
+nota2 = parseFloat(nota2);
 
-// Converter a nota2 de texto para decimal
-nota2 = parseFloat(nota2)
+var nota3 = window.prompt("Digite a nota 3:");
+nota3 = parseFloat(nota3);
 
-// Calculo a média das duas notas
-var media = (nota1 + nota2)/2;
+var nota4 = window.prompt("Digite a nota 4:");
+nota4 = parseFloat(nota4);
 
-//Gero a mensagem de exibição do nome e da média do aluno
+var media = (nota1 + nota2 + nota3 + nota4)+/4;
+
 var mensagem = nome + ", você obteve a média: " + media;
 window.alert(mensagem);
 
+var nome = window.prompt("Informe o nome: ");
+
+var nota1 = window.prompt("Digite a nota 1:");
+nota1 = parseFloat(nota1);
+
+var nota2 = window.prompt("Digite a nota 2:");
+nota2 = parseFloat(nota2);
+
+var nota3 = window.prompt("Digite a nota 3:");
+nota3 = parseFloat(nota3);
+
+var nota4 = window.prompt("Digite a nota 4:");
+nota4 = parseFloat(nota4);
+
+var media = (nota1 + nota2 + nota3 + nota4)+/4;
+
+var mensagem = nome + ", você obteve a média: " + media;
+window.alert(mensagem);
