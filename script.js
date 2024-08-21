657a9c689c293e97c2525c826f59fabb8868f03f
tamyrhuana <tamyres.rsmagalhaes@senacsp.edu.br>
2024-08-20 21:45:04 -0300

    fe

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// Exibir no terminl
 console.log("Olá Mundo");
-
+// Exibir no navegador
 document.write("Olá Navegador! 4");
+// Exibir alerta
+alert("Alerta de Confirmação");
+/*
+Comentario
+de 
+Bloco
+tudo que fica entre /*     /*
+*/
+var nome = window.prompt("Informe o nome:");
+var nota1 = window.prompt("Digite a nota 1:");
+nota1 = parseFloat(nota1);
+var nota2 = window.prompt("Digite a nota 2");
+nota2 = parseFloat(nota2)
+var media = (nota1 + nota2)/2;
+
+var mensagem = nome + ", você obteve a média: " + media;
+window.alert(mensagem);
+
